e1da14fb136cc10466ebc1fa4cb782437b8e1351
gabrielcedran <gabrielcedran@gmail.com>
2022-07-16 18:32:08 +0100

    Validate permission on the server side to not allow users without permission to open the page.

--- fe/hooks/useCan.ts
+++ fe/hooks/useCan.ts
@@ -1,4 +1,5 @@
 import { useAuthContext } from "../contexts/AuthContext"
+import { validateUserPermissions } from "../utils/validateUserPermissions";
 
 type UseCanParams = {
     permissions?: string[],
@@ -13,25 +14,5 @@ export function useCan({permissions, roles}: UseCanParams) {
         return false;
     }
 
-    if (permissions?.length > 0) {
-        const hasAllPermissions = permissions.every(permission => {
-            return user.permissions.includes(permission);
-        })
-
-        if (!hasAllPermissions) {
-            return false;
-        }
-    }
-
-    if (roles?.length > 0) {
-        const hasRole = roles.some(role => {
-            return user.roles.includes(role);
-        })
-
-        if (!hasRole) {
-            return false;
-        }
-    }
-
-    return true;
+    return validateUserPermissions({user, permissions, roles});
 }
--- fe/utils/withSSRAuth.ts
+++ fe/utils/withSSRAuth.ts
@@ -1,17 +1,40 @@
 import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult } from "next";
 import { destroyCookie, parseCookies } from "nookies";
 import { AuthTokenError } from "../errors/AuthTokenError";
+import decode from 'jwt-decode';
+import { validateUserPermissions } from "./validateUserPermissions";
 
-export function withSSRAuth<P>(fn: GetServerSideProps<P>) {
+type WithSSRAuthOptions = {
+    permissions?: string[],
+    roles?: string[],
+}
+
+export function withSSRAuth<P>(fn: GetServerSideProps<P>, options?: WithSSRAuthOptions) {
     return async (ctx: GetServerSidePropsContext): Promise<GetServerSidePropsResult<P>> => {
         const cookies = parseCookies(ctx);
+        const token = cookies['nextAuthApp.token']
 
-        if (!cookies['nextAuthApp.token']) {
+
+        if (!token) {
             return {
                 redirect: {
                     destination: '/',
                     permanent: false,
                 } 
+            }
+        }
+
+        if (options) {
+            const {permissions, roles} = options
+            const user = decode<{permissions: string[], roles: string[]}>(token);
+            if (!validateUserPermissions({user, permissions, roles})) {
+                return {
+                    redirect: {
+                        // notFound - if there is no other place to redirect user
+                        destination: '/',
+                        permanent: false
+                    }
+                }
             }
         }
 

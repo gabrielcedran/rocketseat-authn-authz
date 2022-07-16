3fb76f6e89bffca9b29241ad88d326cbe0198161
gabrielcedran <gabrielcedran@gmail.com>
2022-07-16 11:15:18 +0100

    Create axios interceptor to automatically refresh authz token when it expires

--- fe/services/api.ts
+++ fe/services/api.ts
@@ -9,3 +9,38 @@ export const api = axios.create({
         Authorization: `Bearer ${cookies['nextAuthApp.token']}`
     }
 });
+
+
+// the first parameter is the logic to be applied upon success
+// the second parameter is the logic to be applied upon error 
+api.interceptors.response.use(response => response, (error: AxiosError<any>) => {
+    if (error.response.status === 401) {
+        if (error.response.data?.code === 'token.expired') {
+
+            // refresh the cookies to make sure the newest one is being used instead of the one that was loaded with the app
+            cookies = parseCookies();
+            
+            const {'nextAuthApp.refreshToken': refreshToken} = cookies;
+
+            api.post('/refresh', {
+                refreshToken
+            }).then(response => {
+                const {token: newToken, refreshToken: newRefreshToken} = response.data;
+                
+                setCookie(undefined, 'nextAuthApp.token', newToken, {
+                    maxAge: 60 * 60 * 24 * 30,
+                    path: '/'
+                })
+                setCookie(undefined, 'nextAuthApp.refreshToken', newRefreshToken, {
+                    maxAge: 60 * 60 * 24 * 30, 
+                    path: '/'
+                })
+
+                // default headers need to be updated otherwise it would use the one loaded during initialization
+                api.defaults.headers['Authorization'] = `Bearer ${newToken}`
+            })
+        } else {
+
+        }
+    }
+})
